3cf4fea63f3d93417eb8152fdaf21153cc4adb56
Scarlet Puma <scarlt.puma@gmail.com>
2022-02-16 19:39:38 -0500

    css adjustments

--- models/reviews.js
+++ models/reviews.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const reviewSchema = new mongoose.Schema({
   videoGameTitle: {type: String, required: true},
+  author: {type: String, required: true},
   rate: {type: String, required: true},
   review: {type: String}
 })
